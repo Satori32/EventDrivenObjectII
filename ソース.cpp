bce0f302ec098dcbbdc364ebb48e14e65b47622e
Satori32 <Huga8989+githubf@gmail.com>
2021-02-01 21:08:48 +0900

    add method.

--- ソース.cpp
+++ ソース.cpp
@@ -2,11 +2,17 @@
 #include <map>
 #include <functional>
 #include <algorithm>
+#include <cstdint>
 
 template<class Data,class Event=int>
 class EventDrivenObject {
 public:
 	typedef std::function<bool(EventDrivenObject&)> Fun;
+
+	bool Initialize(const Data& In) {
+		D = In;
+		return true;
+	}
 
 	bool Add(const Event& E, const Fun& F) {
 		M[E] = F;
@@ -45,16 +51,25 @@ enum class Event {
 	Huga,
 };
 
-int main() {
-	EventDrivenObject<int, Event> EDO;
+struct  Data{
+	std::uint64_t X;
+	std::uint64_t Y;
+	double Z;
+};
 
-	EDO.Add(Event::Init, [](decltype(EDO)& o)->bool {o.GetData() = 0; return true; });
+int main() {
+	//EventDrivenObject<std::uint64_t, Event> EDO;
+	EventDrivenObject<Data, Event> EDO;
+
+	EDO.Initialize({0xdeadbeef,1,2});
+
+	EDO.Add(Event::Init, [](decltype(EDO)& o)->bool {o.GetData().X = 0; return true; });
 
 	EDO.Trigger(Event::Init);
 
 	EDO.Erase(Event::Init);
 
-	std::cout << EDO.GetData() << std::endl;
+	std::cout << EDO.GetData().X << std::endl;
 
 	return 0;
 }
